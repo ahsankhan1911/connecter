be6fb151a32eb495f7f52a1cbfb5152dd469d157
ahsankhan1911 <ahsankhan1911@gmail.com>
2017-05-02 12:59:45 +0500

    Basic Implementation done

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 
-
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 // const bodyParser = require ('body-parser');
 // const mongoose = require('mongoose');
 const server = require('http').createServer(app);
-//const io = require('socket.io').listen(server);
+const io = require('socket.io').listen(server);
 //const Boom  = require('boom');
 
 users = [];
@@ -16,29 +15,49 @@ connections = [];
 // app.use(bodyParser.json()); // support json encoded bodies
 // app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-
-
-
 //app.use('/users', require('./user'));
 
 server.listen(port);
     console.log(`Running server on ${port}`);
 
+
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-  });  
-
-     io.sockets.on('connection' , (socket) => {
-    connections.push(socket);
-    console.log("Connected: %s sockets connected", connections.length);
-
-
-//disconnect
-
-    connections.splice(connections.indexOf(socket), 1);
-    console.log("Disconnected: %s sockets connected", connections.length);
-
+res.sendFile(__dirname+'/index.html'); 
 });
 
-   
+io.sockets.on('connection' , (socket) => {
+connections.push(socket);
+console.log("Connected: %s sockets connected", connections.length);
+ 
+//disconnect
+  socket.on('disconnected', (data) => {
+
+users.splice(users.indexOf(socket.username), 1)
+   connections.splice(connections.indexOf(socket), 1);
+   updateUsernames();
+    console.log("Disconnected: %s sockets connected", connections.length);
+  });
+ 
+ //Send Message\
+
+ socket.on('send message', (data) => {
+     
+     io.sockets.emit('new message', {msg: data, user: socket.username});
+ });
+
+ //New user 
+
+ socket.on('new user', (data, callback) => {
+callback(true);
+
+socket.username = data;
+users.push(socket.username);
+updateUsernames();
+ });
+
+function  updateUsernames() {
+    io.sockets.emit('get users', users)
+}
+
+});
 
